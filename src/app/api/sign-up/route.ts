71635aa6c189f1776c0386a94790731257f3aa1f
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-06-03 23:07:03 +0530

    update code

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -12,11 +12,40 @@ export async function POST(request: Request) {
         try {
            const {username, email , password} =  await request.json();
 
-           const existingUserVerifiedByUsername = UserModel.findOne({
+           const existingUserVerifiedByUsername = await UserModel.findOne({
             username,
             isVerified: true
            })
 
+           if(existingUserVerifiedByUsername){
+            return Response.json({
+                success: false,
+                message: "Username already taken"
+            }, {
+                status:400
+            })
+           }
+           const exisitngUserByEmail = await UserModel.findOne({email})
+
+           if(exisitngUserByEmail){
+
+           }
+           else{
+            const hasedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+            new UserModel({
+                username,
+                email,
+                password : hasedPassword,   
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified : false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+            
+           }
         } catch (error) {
             console.error("Error registering user", error);
             return Response.json({
